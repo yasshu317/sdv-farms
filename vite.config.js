f2f1c72482bcdaa155ee623752c1a7d92d942009
yaswanth <yasshu317@users.noreply.github.com>
2026-03-20 11:54:23 -0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,5 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-
-// Change 'sdv-farms' to your actual GitHub repo name if different
 export default defineConfig({
   plugins: [react()],
-  base: '/sdv-farms/',
 })
